7c2c62027ece1365a706125961097d6bf2a1ac53
Jam Risser <jam@codejam.ninja>
2018-12-06 14:37:12 -0600

    Detect cyclical cycles

--- packages/module-loader/src/index.js
+++ packages/module-loader/src/index.js
@@ -1,3 +1,4 @@
+import TrailDuck from 'trailduck';
 import _ from 'lodash';
 import path from 'path';
 import pkgDir from 'pkg-dir';
@@ -11,8 +12,9 @@ export default class ModuleLoader {
   name = '';
 
   constructor(name, options = {}) {
-    const { configPath = '' } = options;
+    const { configPath = '', dependsOnPath = '' } = options;
     this.configPath = configPath;
+    this.dependsOnPath = dependsOnPath;
     this.name = name;
   }
 
@@ -32,9 +34,26 @@ export default class ModuleLoader {
     return this._moduleNames;
   }
 
+  getModulesGraph(modules) {
+    return _.reduce(
+      modules,
+      (graph, module, moduleName) => {
+        const children = this.dependsOnPath?.length
+          ? module.config[this.dependsOnPath] || []
+          : [];
+        graph[moduleName] = { children };
+        _.each(children, child => {
+          if (!graph[child]) graph[child] = { children: [] };
+        });
+        return graph;
+      },
+      {}
+    );
+  }
+
   get modules() {
     if (this._modules) return this._modules;
-    this._modules = _.reduce(
+    const modules = _.reduce(
       this.moduleNames,
       (modules, moduleName) => {
         modules[moduleName] = new Module(moduleName, this.name, {
@@ -44,6 +63,34 @@ export default class ModuleLoader {
       },
       {}
     );
+    const graph = this.getModulesGraph(modules);
+    const trailDuck = new TrailDuck(graph);
+    const missingDependancies = _.xor(
+      this.moduleNames,
+      _.map(trailDuck.ordered, 'name')
+    );
+    if (missingDependancies.length) {
+      throw new Error(
+        `plugin dependencies missing '${missingDependancies.join("', '")}'`
+      );
+    }
+    if (trailDuck.cycles.length) {
+      const cyclicalDependancies = _.uniq(_.flatten(trailDuck.cycles));
+      throw new Error(
+        `cyclical plugin dependencies detected '${cyclicalDependancies.join(
+          "', '"
+        )}'`
+      );
+    }
+    const orderedModules = _.reduce(
+      _.reverse(_.map(trailDuck.ordered, 'name')),
+      (orderedModules, moduleName) => {
+        orderedModules[moduleName] = modules[moduleName];
+        return orderedModules;
+      },
+      {}
+    );
+    this._modules = orderedModules;
     return this._modules;
   }
 }
